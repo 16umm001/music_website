f2debdd32bddceb7b2a110104416583e6e14a102
16umm001 <adarshp199877@gmail.com>
2021-03-23 13:30:03 +0530

    album title size changes from 500 to 400

--- music/models.py
+++ music/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 class Album(models.Model):
     artist = models.CharField(max_length=250)
-    album_title = models.CharField(max_length=500)
+    album_title = models.CharField(max_length=400)
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
     def __str__(self):
